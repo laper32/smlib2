5302292f97a2dade001cbd96a394ecc790c71f32
laper32 <laper32@outlook.com>
2021-04-24 10:43:58 +0100

    ehh maybe should have an output file here...

--- ci/test.sh
+++ ci/test.sh
@@ -62,7 +62,7 @@ done
 cp test.sp addons/sourcemod/scripting/test.sp
 cp -rf include/* addons/sourcemod/scripting/include
 
-addons/sourcemod/scripting/spcomp -E -v2 -O2 addons/sourcemod/scripting/test.sp
+addons/sourcemod/scripting/spcomp -E -v2 -O2 addons/sourcemod/scripting/test.sp -o"addons/sourcemod/scripting/test.smx"
 if [ ! -f "addons/sourcemod/scripting/test.smx" ]; then
     echo "Build: test.smx failed. Check error output"
     exit 1
